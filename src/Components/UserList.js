ad14ecb6b3c8967da74cbebde6a193ec97ac6ac7
Your Name <aiswaryacs34@gmail.com>
2024-03-30 23:58:44 +0530

    inital commit

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { Users } from '../utils/Config';
 
-const UserList = () => {
+const UserList = ({setHideandshow}) => {
 
   const [userlist, setUserlist] = useState([]);
 
   useEffect(() => {
-    getuserList()
-  }, []);
+    getuserList();
+    setHideandshow(false);
+    return () => {
+      setHideandshow(true);
+    }
+  }, [setHideandshow])
+  
   
   const getuserList = async () => {
     const data = await fetch(Users);
@@ -17,11 +22,33 @@ const UserList = () => {
   }
   
   return (
-    <div>
-      <h1>Our Customers</h1>
-      
+    <div className='w-[90%] lg:h-auto my-5 mx-10 items-center'>
+      <table className="table-auto border border-1 w-full">
+        <thead className="border border-1">
+          <tr>
+            <th className="border border-1 ">Avatar</th>
+            <th className="border border-1">Name</th>
+            <th className="border border-1">Email</th>
+            <th className="border border-1">Role</th>
+          </tr>
+        </thead>
+        {userlist.map((list,id)=>{
+          return(
+            <tbody>
+          <tr className="border border-1">
+            <td className="border border-1 w-20 h-20">{list.avatar}</td>
+            <td className="border border-1">{list.name}</td>
+            <td className="border border-1">{list.email}</td>
+            <td className="border border-1">{list.role}</td>
+          </tr>
+          
+        </tbody>
+          )
+        })}
+        
+      </table>
     </div>
   )
 }
 
-export default UserList
+export default UserList;
